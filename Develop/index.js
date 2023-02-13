6f0d2e081687add08941220db562026f996d2f73
Masyn N <masynnay33@gmail.com>
2023-02-12 21:57:41 -0700

    Edited js
    edited js to edit the comment section

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
@@ -58,12 +58,12 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
   inquirer.prompt(questions).then((data) => {
     console.log('Creating README.');
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
   let badge = ``;
   switch (license) {
@@ -17,8 +16,7 @@ function renderLicenseBadge(license) {
   }
   return badge;
 }
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
 function renderLicenseLink(license) {
   let link = ``;
   switch (license) {
@@ -37,8 +35,7 @@ function renderLicenseLink(license) {
   return link;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
 function renderLicenseSection(license) {
   let section = ``;
   if(license === 'None') {
@@ -50,7 +47,7 @@ function renderLicenseSection(license) {
   return section;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
